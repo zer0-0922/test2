96f2d822742c86132d34cda1b0059601ea580d1f
yamagishi_yuki <76723541+zer0-0922@users.noreply.github.com>
2024-01-17 00:54:08 +0900

    test start

---
+++ test.cs
@@ -0,0 +1,19 @@
+/// <summary>
+/// Git テスト操作用クラス
+/// </summary>
+class Test
+{
+    static void Main(string[] args)
+    {
+        ShowMessage();
+    }
+    /// <summary>
+    /// メッセージを表示
+    /// </summary>
+    public static void ShowMessage()
+    {
+        //以下の文字列を変更してください
+        Console.WriteLine("Thank you!");
+        Console.WriteLine("Hello World!");
+    }
+}
